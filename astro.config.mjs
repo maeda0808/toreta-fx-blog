16ae4380f55f7d99c5ba124bc4c8f2f6bc6826c4
maeda0808 <sinsuke.maeda.88@gmail.com>
2025-12-10 23:00:25 +0900

    Fix: Add MDX integration to support Keystatic content

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,12 @@ import keystatic from '@keystatic/astro';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [
-    react(),
-    keystatic()
-  ],
+  integrations: [react(), keystatic(), mdx()],
 
   adapter: cloudflare()
 });
